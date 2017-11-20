461d74d5070f25fb93024c5afe758fc75072d549
Sander Ronde <awsdfgvhbjn@gmail.com>
2017-11-20 03:22:39 +0100

    completed docs page

--- docs/assets/js/js.ts
+++ docs/assets/js/js.ts
@@ -16,35 +16,39 @@ interface Window {
 			ts: monaco.editor.IModel;
 			html: monaco.editor.IModel;
 		};
+		states: {
+			ts: monaco.editor.ICodeEditorViewState;
+			html: monaco.editor.ICodeEditorViewState;
+		}
 		editor: monaco.editor.IStandaloneCodeEditor;
 	}
 	tryEditor: {
 		models: {
-			js: monaco.editor.IModel;
+			ts: monaco.editor.IModel;
 		};
 		editor: monaco.editor.IStandaloneCodeEditor;
 	};
 	defs: {
 		update(defs: string): void;
-		currentDefs: string;
+		currentDefsString: string;
+		currentDefsDisposable: monaco.IDisposable;
 	}
+	exports: Object;
+	htmlTypings: HTMLTypings;
 }
 
 const DEFAULT_CONTENTS = {
 	source: {
-		ts: (() => {
-/// <reference path="node_modules/html-typings/typings/html-typings.d.ts"/>
+		ts: `//Import HTML-typings
+import * as htmlTypings from 'html-typings';
+
+//Get the HTML source code
 const sourceCode = window.sourceEditor.models.html.getValue();
 
-const htmlTypings: HTMLTypings = require('html-typings');
+//Generate defs file
 const defsFile = htmlTypings.extractStringTypes(sourceCode);
 
-//This is where you would normally write it to a .d.ts file or use the CLI instead.
-// However, because this is just a browser example, we'll just update the global
-// defs instead
-window.defs.update(defsFile);
-
-/*
+//Write away definitions
 require('fs').writeFile('src/defs.d.ts', defsFile, {
 	encoding: 'utf-8'
 }, (err: Error) => {
@@ -53,11 +57,8 @@ require('fs').writeFile('src/defs.d.ts', defsFile, {
 	} else {
 		//Done
 	}
-});
-*/
-		}).toString(),
-		html: `
-<html>
+});`,
+		html: `<html>
 	<head>
 		<title>html-typings</title>
 	</head>
@@ -68,12 +69,64 @@ require('fs').writeFile('src/defs.d.ts', defsFile, {
 		<div id="specialEl" data-element-type="MySpecialType"></div>
 		<dom-module id="my-element">
 			<h1 id="header">{{header}}</h1>
+			<my-other-element id="otherElement"></my-other-element>
+		</dom-module>
+		<dom-module id="my-other-element">
+			<h2 id="header">{{header}}</h2>
 		</dom-module>
 	</body>
 </html>`
 	},
 	try: {
-		ts: ``
+		ts: `// The generated HTML typings file is automatically included,
+//  normally you'd use ///<reference path="defs.d.ts" /> for this
+
+// Type will be HTMLInputElement
+const input = document.getElementById('someInput');
+
+// Type will be NodeListOf<HTMLDivElement|HTMLSpanElement>
+const fancyElements = document.getElementsByClassName('fancy');
+
+// Type will also be HTMLInputElement
+const sameInput = document.querySelector('#someInput');
+
+// Type will also be NodeListOf<HTMLDivElement|HTMLSpanElement>
+const fancyElementsCopy = document.querySelectorAll('.fancy');
+
+// Some custom element libraries allow for element-specific ID selectors
+// for example polymer:
+// https://www.polymer-project.org/1.0/docs/devguide/local-dom#work-with-local-dom 
+type Modules = keyof ModuleMap;
+type CustomElement<T extends Modules> = Element & {
+	$: ModuleMap[T]
+};
+
+type HTMLMyElementElement = CustomElement<'my-element'>;
+type HTMLMyOtherElementElement = CustomElement<'my-other-element'>;
+
+// Will be a custom element
+const myElement = document.getElementsByTagName('my-element')[0];
+
+// Type will be HTMLHeaderElement
+const header = myElement.$.header;
+
+// Gets the nested element
+const nestedHeader = myElement.$.otherElement.$.header;
+
+
+interface MySpecialType extends HTMLElement {
+	func(): void;
+	color: string;
+}
+
+// Type will be MySpecialType
+const specialElement = document.getElementById('specialEl');
+
+// Instead of this for every occurrence
+const specialElementManual = document.getElementById('specialEl') as HTMLElement & {
+	func(): void;
+	color: string;
+}`
 	}
 };
 
@@ -95,7 +148,7 @@ require('fs').writeFile('src/defs.d.ts', defsFile, {
 	waitFor('require', () => {
 		window.require.config({
 			paths: {
-				'vs': '/assets/monaco-editor/src/min/vs'
+				'vs': '/assets/monaco-editor/min/vs'
 			}
 		});
 		//Load the editor
@@ -105,8 +158,28 @@ require('fs').writeFile('src/defs.d.ts', defsFile, {
 	});
 })(() => {
 	//Make it global so the user can play with it
-	(() => {
-		const jsModel = monaco.editor.createModel(DEFAULT_CONTENTS.source.ts, 'typescript');
+	const sourceEditor = (() => {
+		function changeTab(stateName: 'ts'|'html') {
+			const currentState = editor.saveViewState();
+			const currentModel = editor.getModel();
+			if (currentModel === tsModel) {
+				window.sourceEditor.states.ts = currentState;
+				window.sourceEditor.editor.updateOptions({
+					readOnly: false
+				});
+			} else if (currentModel === htmlModel) {
+				window.sourceEditor.states.html = currentState;
+				window.sourceEditor.editor.updateOptions({
+					readOnly: true
+				});
+			}
+
+			editor.setModel(window.sourceEditor.models[stateName]);
+			editor.restoreViewState(window.sourceEditor.states[stateName]);
+			editor.focus();
+		}
+
+		const tsModel = monaco.editor.createModel(DEFAULT_CONTENTS.source.ts, 'typescript');
 		const htmlModel = monaco.editor.createModel(DEFAULT_CONTENTS.source.html, 'html');
 		const editor = monaco.editor.create(document.getElementById('sourceEditor'), {
 			model: htmlModel,
@@ -115,12 +188,156 @@ require('fs').writeFile('src/defs.d.ts', defsFile, {
 			}
 		});
 
+		const htmlTab = document.getElementById('sourceHTMLTab');
+		const tsTab = document.getElementById('sourceTSTab');
+		htmlTab.addEventListener('click', () => {
+			if (htmlTab.classList.contains('active')) {
+				return;
+			}
+			htmlTab.classList.add('active');
+			tsTab.classList.remove('active');
+			changeTab('html');
+		});
+		tsTab.addEventListener('click', () => {
+			if (tsTab.classList.contains('active')) {
+				return;
+			}
+			tsTab.classList.add('active');
+			htmlTab.classList.remove('active');
+			changeTab('ts');
+		});
+
 		window.sourceEditor = {
 			editor: editor,
 			models: {
 				html: htmlModel,
-				ts: jsModel
+				ts: tsModel
+			},
+			states: {
+				html: null,
+				ts: null
+			}
+		}
+
+		let hasChanged: boolean = false;
+		editor.onDidChangeModelContent(() => {
+			hasChanged = true;
+		});
+		const returnVal: {
+			onChange: () => void;
+		} = {
+			onChange: null
+		};
+		window.setInterval(() => {
+			if (hasChanged) {
+				returnVal.onChange && returnVal.onChange();		
+				hasChanged = false;
+			}
+		}, 250);
+		return returnVal;
+	})();
+
+	const tryEditor = (() => {
+		const tsModel = monaco.editor.createModel(DEFAULT_CONTENTS.try.ts, 'typescript');
+		const editor = monaco.editor.create(document.getElementById('tryEditor'), {
+			model: tsModel,
+			minimap: {
+				enabled: false
+			}
+		});
+
+		editor.addOverlayWidget
+
+		window.tryEditor = {
+			editor: editor,
+			models: {
+				ts: tsModel
+			}
+		}
+
+		window.defs = {
+			update(defs: string) {
+				window.defs.currentDefsString = defs;
+			},
+			currentDefsString: '',
+			currentDefsDisposable: null
+		}
+
+		const widgetElement = document.createElement('div') as HTMLDivElement & {
+			timer: number;
+		};
+		widgetElement.innerText = 'Updated ✓';
+		widgetElement.classList.add('onUpdateWidget');
+		editor.addOverlayWidget({
+			getId() {
+				return 'on-update-widget'
+			},
+			getDomNode() {
+				return widgetElement;
+			},
+			getPosition() {
+				return {
+					preference: monaco.editor.OverlayWidgetPositionPreference.BOTTOM_RIGHT_CORNER
+				}
+			}
+		});
+
+
+		return {
+			updateDefs(showNotification: boolean = true) {
+				window.exports = {};
+				const htmlFile = window.sourceEditor.models.html.getValue();
+				window.require(['./assets/html-typings/app/index.js'], () => {
+					debugger;
+					const defs = window.htmlTypings.extractStringTypes(htmlFile);
+					
+					window.defs.update(defs);
+					const clearDefs = window.defs.currentDefsDisposable;
+					clearDefs && clearDefs.dispose();
+	
+					window.defs.currentDefsDisposable = 
+						monaco.languages.typescript.typescriptDefaults
+							.addExtraLib(defs, 'htmlTypings.d.ts');
+
+					if (showNotification) {
+						widgetElement.classList.add('active');
+						const currentTimer = window.setTimeout(() => {
+							if (widgetElement.timer === currentTimer) {
+								widgetElement.classList.remove('active');
+							}
+						}, 2500);
+						widgetElement.timer = currentTimer;
+					}
+				});
 			}
 		}
 	})();
+
+	(() => {
+		function loadDefs(path: string, name: string) {
+			const xhr = new XMLHttpRequest();
+			xhr.open('GET', `${location.origin}${path}`);
+			xhr.onload = () => {
+				if (xhr.readyState === xhr.DONE) {
+					if (xhr.status === 200) {
+						monaco.languages.typescript.typescriptDefaults
+							.addExtraLib(xhr.responseText, name);
+					} else {
+						console.error(`Failed to load ${name}`);
+					}
+				}
+			}
+			xhr.send();
+		}
+
+		loadDefs('/assets/monaco-editor/monaco.d.ts', 'monaco.d.ts');
+		loadDefs('/assets/html-typings/typings/html-typings.d.ts', 'html-typings.d.ts');
+		monaco.languages.typescript.typescriptDefaults.addExtraLib('interface Window {	require: {		(paths: Array<string>, callback: () => void): void;		config(config: {			paths: {				[key: string]: string;			}		}): void;	};	sourceEditor: {		models: {			ts: monaco.editor.IModel;			html: monaco.editor.IModel;		};		states: {			ts: monaco.editor.ICodeEditorViewState;			html: monaco.editor.ICodeEditorViewState;		}		editor: monaco.editor.IStandaloneCodeEditor;	}	tryEditor: {		models: {			ts: monaco.editor.IModel;		};		editor: monaco.editor.IStandaloneCodeEditor;	};	defs: {		update(defs: string): void;		currentDefsString: string;		currentDefsDisposable: monaco.IDisposable;	}}', 'window.d.ts');
+		monaco.languages.typescript.typescriptDefaults.addExtraLib('declare const require: (module: string) => any;', 'require.d.ts');
+	})();
+
+	sourceEditor.onChange = () => {
+		tryEditor.updateDefs();
+	}
+	tryEditor.updateDefs(false);
 });
