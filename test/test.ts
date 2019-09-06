3ee1c7a44c44780549f81878bb04d71672bd452f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-06 20:52:48 +0000

    Deploy SanderRonde/html-typings to github.com/SanderRonde/html-typings.git:gh-pages

--- test/test.ts
+++
@@ -1,9 +0,0 @@
-process.env.DEBUG_HTML_TYPINGS = 'true';
-import { programmaticTests } from './programmatic/test'
-import { cliTests } from './cli/test';
-
-
-describe('HTMLTypings', () => {
-	programmaticTests();
-	cliTests();
-});
